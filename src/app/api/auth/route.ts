18c6dfcd9a7c2142bbf589f0d69c50339774e57f
Rohan79912 <rohankushwaha582@gmail.com>
2026-01-22 23:55:33 +0530

    Initial commit

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,14 @@
+[{
+	"resource": "/c:/Users/VICTUS/OneDrive/Desktop/hackethon/linkz/src/app/route.ts",
+	"owner": "typescript",
+	"code": "2307",
+	"severity": 8,
+	"message": "Cannot find module '@/models/projectOrResearch' or its corresponding type declarations.",
+	"source": "ts",
+	"startLineNumber": 3,
+	"startColumn": 31,
+	"endLineNumber": 3,
+	"endColumn": 59,
+	"modelVersionId": 19,
+	"origin": "extHost1"
+}]
